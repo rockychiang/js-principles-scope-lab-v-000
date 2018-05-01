fa1d6ed74ab0487f5996418a257b5b025b00b94b
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-01 00:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(customerName) {
-  return name = customerName.toUpperCase();
+  customerName = customerName.toUpperCase();
 }
 
 function setBestCustomer() {
